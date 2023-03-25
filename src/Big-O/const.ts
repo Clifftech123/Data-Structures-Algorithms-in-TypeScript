359870cc8351ab2a78d7550d144478947dd3316e
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-03-25 18:04:08 +0000

    Adding constant time complexcity

---
+++ src/Big-O/const.ts
@@ -0,0 +1,24 @@
+// THIS IS CONSTANT TIME COMPLEXITY BECAUSE THE TIME IT TAKES TO RUN THE FUNCTION IS NOT DIRECTLY PROPORTIONAL TO THE SIZE OF THE INPUT 
+
+// 0(1) --> Constant Time 
+
+// crating an array with one element
+
+const constant:string[] = ["Clifford", "Isaiah","Opoku"];
+
+// Functuon that get the array and prints the first element of the array
+
+function getFirstElement(array:string[]) :void {
+
+  console.log(array[0]);
+
+}
+
+// calling the function
+
+getFirstElement(constant); // Clifford 
+
+
+
+//  This is and example of a constant time complexity algorithm
+
